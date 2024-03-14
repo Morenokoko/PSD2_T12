7e4366375035eb48a1be55aa6e018461eb589449
Morenokoko <kokomoreno1010@gmail.com>
2024-03-15 02:20:47 +0800

    remove redundant Tempdata file

--- frontend/app/src/main/java/com/example/ecoranger/HomePage.kt
+++ frontend/app/src/main/java/com/example/ecoranger/HomePage.kt
@@ -37,6 +37,7 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
+import com.example.ecoranger.data.activityList
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
--- frontend/app/src/main/java/com/example/ecoranger/data/Activity.kt
+++ frontend/app/src/main/java/com/example/ecoranger/data/Activity.kt
@@ -3,13 +3,21 @@ package com.example.ecoranger.data
 data class Activity(
     val date: String,
     val time: String,
+    val location: String,
     val points: Int
 )
 
 val activityList = listOf(
-    Activity("2024-03-01", "10:00 AM", 100),
-    Activity("2024-03-02", "11:30 AM", 150),
-    Activity("2024-03-03", "09:45 AM", 200),
-    Activity("2024-03-04", "02:15 PM", 50),
-    // Add more activities as needed
+    Activity("13 Mar 2024", "10:00 AM", "Recycling Bin - Sengkang", 100),
+    Activity("12 Mar 2024", "11:30 AM", "Recycling Bin - Waterway Point", 150),
+    Activity("11 Mar 2024", "09:45 AM", "Recycling Bin - Fernvale", 200),
+    Activity("10 Mar 2024", "02:15 PM", "Recycling Bin - Sengkang", 50),
+    Activity("10 Mar 2024", "02:15 PM", "Recycling Bin - Sengkang", 80),
+    Activity("10 Mar 2024", "02:15 PM", "Recycling Bin - Sengkang", 120),
+    Activity("10 Mar 2024", "02:15 PM", "Recycling Bin - Sengkang", 40),
+    Activity("10 Mar 2024", "02:15 PM", "Recycling Bin - Sengkang", 60),
+    Activity("10 Mar 2024", "02:15 PM", "Recycling Bin - Sengkang", 70),
+    Activity("10 Mar 2024", "02:15 PM", "Recycling Bin - Sengkang", 30),
+    Activity("10 Mar 2024", "02:15 PM", "Recycling Bin - Sengkang", 90),
+//  Add more activities as needed
 )
